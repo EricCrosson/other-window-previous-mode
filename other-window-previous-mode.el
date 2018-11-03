a9a2fedbd801ff4e31a55d1cd9a442648c11d113
Eric Crosson <eric.s.crosson@utexas.com>
2018-11-03 11:34:02 -0500

    Initial working version

---
+++ other-window-previous-mode.el
@@ -0,0 +1,57 @@
+;;; other-window-previous-mode.el --- Global minor-mode defining `other-window' backwards
+;;
+;;; Copyright (C) 2018  Free Software Foundation, Inc.
+;;
+;; Author: Eric Crosson <eric.s.crosson@utexas.edu>
+;; Version: 0.0.1
+;; Keywords: conveniencely
+;; Package-Requires: ((emacs "24"))
+;; URL: https://github.com/EricCrosson/other-window-previous-mode
+;;
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+;;
+;;
+;;; Commentary:
+;;
+;; Provides a minor-mode defining `other-window' backwards to "C-x O"
+;; (capital O).
+
+;;; Code:
+
+
+(defun other-window-previous ()
+  "Invoke `other-window' backwards."
+  (interactive)
+  (other-window -1))
+
+
+;;;###autoload
+(define-minor-mode other-window-previous-mode
+  "Toggle Other Window Previous mode.
+This mode provides a binding to invoke `other-window' backwards."
+  :init-value nil
+  :lighter nil
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-x O") 'other-window-previous)
+            map)
+  :global t
+  :group 'other-window-previous-mode
+  :require 'other-window-previous-mode)
+
+(provide 'other-window-previous-mode)
+
+;;; other-window-previous-mode.el ends here
---
+++ readme.md
@@ -0,0 +1,37 @@
+# other-window-previous-mode [![Build Status](https://travis-ci.org/EricCrosson/other-window-previous-mode.svg?branch=master)](https://travis-ci.org/EricCrosson/other-window-previous-mode)
+
+> GNU Emacs `other-window` backwards
+
+This package provides the `other-window-previous-mode` minor-mode,
+which includes a single function (`other-window-previous`) and binds
+it to `C-x O` (that's capital `o`).
+
+## Install
+
+With [Quelpa](https://framagit.org/steckerhalter/quelpa)
+
+``` {.sourceCode .lisp}
+(use-package other-window-previous-mode
+  :quelpa (other-window-previous-mode
+           :fetcher github
+           :repo "EricCrosson/other-window-previous-mode")
+  :config (other-window-previous-mode 1))
+```
+
+Or manually, after downloading into your `load-path`
+
+``` {.sourceCode .lisp}
+(require 'other-window-previous-mode)
+(other-window-previous-mode 1)
+```
+
+## Related
+
+## Acknowledgements
+
+This package is a modular implementation of a
+[Prelude feature](http://emacsredux.com/blog/2013/03/30/go-back-to-previous-window/).
+
+## License
+
+GPL 2 (or higher) © [Free Software Foundation, Inc](http://www.fsf.org/about).
