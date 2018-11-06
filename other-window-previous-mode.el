696b442b22f64117de023e561ab3a83c86c84d32
Eric Crosson <eric.s.crosson@utexas.com>
2018-11-05 22:29:11 -0600

    Publish version 1.0.1

--- other-window-previous-mode.el
+++ other-window-previous-mode.el
@@ -3,7 +3,7 @@
 ;;; Copyright (C) 2018  Free Software Foundation, Inc.
 ;;
 ;; Author: Eric Crosson <eric.s.crosson@utexas.edu>
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "24"))
 ;; URL: https://github.com/EricCrosson/other-window-previous-mode
